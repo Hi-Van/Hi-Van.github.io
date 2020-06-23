cf0109b27474ebe07016a09ee7347ee805d19ea5
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2020-06-23 13:37:32 -0400

    Abandoned React App

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import { backVid } from './backgroundVideo.mp4';
 
 function App() {
   return (
     <div className="App">
-      
+      <video src="backgroundVideo.mp4" controls="controls" autoplay="true" loop="true" type="video/mp4" />
     </div>
   );
 }
